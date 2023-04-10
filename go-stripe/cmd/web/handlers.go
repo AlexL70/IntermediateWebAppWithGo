0c5e469479c66a5dd97dab2c130fa756f671e6be
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-04-10 16:40:29 +0300

    25. Generating a receipt

--- go-stripe/cmd/web/handlers.go
+++ go-stripe/cmd/web/handlers.go
@@ -12,3 +12,35 @@ func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) 
 		app.errorLog.Println(err)
 	}
 }
+
+func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	// read posted data
+	cardHolder := r.Form.Get("cardholder_name")
+	email := r.Form.Get("cardholder_email")
+	paymentIntent := r.Form.Get(("payment_intent"))
+	paymentMethod := r.Form.Get(("payment_method"))
+	paymentAmount := r.Form.Get(("payment_amount"))
+	paymentCurrency := r.Form.Get(("payment_currency"))
+
+	data := map[string]any{
+		"cardHolder": cardHolder,
+		"email":      email,
+		"pi":         paymentIntent,
+		"pm":         paymentMethod,
+		"pa":         paymentAmount,
+		"pc":         paymentCurrency,
+	}
+
+	if err := app.renderTemplate(w, r, "succeeded", &templateData{
+		Data: data,
+	}); err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+}
