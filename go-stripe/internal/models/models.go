9dd2dc6cc26591461137d93f7a27276502942689
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-04-12 21:21:40 +0300

    42. Inserting a new order

--- go-stripe/internal/models/models.go
+++ go-stripe/internal/models/models.go
@@ -122,7 +122,29 @@ func (m *DBModel) InsertTransaction(txn Transaction) (int, error) {
 	}
 	id, err := result.LastInsertId()
 	if err != nil {
-		return 0, fmt.Errorf("error getting last inserted id: %w", err)
+		return 0, fmt.Errorf("error getting transaction's last inserted id: %w", err)
+	}
+	return int(id), nil
+}
+
+// InsertOrder inserts new order and returns it's id
+func (m *DBModel) InsertOrder(order Order) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `
+		insert into orders (widget_id, transaction_id, status_id, quantity,
+			amount, created_at, updated_at)	
+		values(?,?,?,?,?,?,?)
+	`
+	result, err := m.DB.ExecContext(ctx, stmt, order.WidgetID, order.TransactionID, order.StatusID, order.Quantity,
+		order.Amount, time.Now(), time.Now())
+	if err != nil {
+		return 0, fmt.Errorf("error adding order: %w", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("error getting order's last inserted id: %w", err)
 	}
 	return int(id), nil
 }
