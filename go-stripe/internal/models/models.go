a02c14d5396bf6919a4da924f1c079e2d6cf8082
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-04-17 16:12:26 +0300

    47. Create the save customer database method

--- go-stripe/internal/models/models.go
+++ go-stripe/internal/models/models.go
@@ -3,6 +3,7 @@ package models
 import (
 	"context"
 	"fmt"
+	"reflect"
 	"time"
 
 	"gorm.io/gorm"
@@ -25,115 +26,125 @@ func NewModels(db *gorm.DB) Models {
 	}
 }
 
+type IDBEntity interface {
+	GetID() int
+}
+type DBEntity struct {
+	ID        int       `json:"id"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
+
+func (e DBEntity) GetID() int {
+	return e.ID
+}
+
 // Widget is the type for all widgets
 type Widget struct {
-	ID             int       `json:"id"`
-	Name           string    `json:"name"`
-	Description    string    `json:"description"`
-	InventoryLevel int       `json:"inventory_level"`
-	Price          int       `json:"price"`
-	Image          string    `json:"image"`
-	CreatedAt      time.Time `json:"-"`
-	UpdatedAt      time.Time `json:"-"`
+	DBEntity
+	Name           string `json:"name"`
+	Description    string `json:"description"`
+	InventoryLevel int    `json:"inventory_level"`
+	Price          int    `json:"price"`
+	Image          string `json:"image"`
 }
 
 // Order is the type for all orders
 type Order struct {
-	ID            int       `json:"id"`
-	WidgetID      int       `json:"widget_id"`
-	TransactionID int       `json:"transaction_id"`
-	CustomerID    int       `json:"customer_id"`
-	StatusID      int       `json:"status_id"`
-	Quantity      int       `json:"quantity"`
-	Amount        int       `json:"amount"`
-	CreatedAt     time.Time `json:"-"`
-	UpdatedAt     time.Time `json:"-"`
+	DBEntity
+	WidgetID      int `json:"widget_id"`
+	TransactionID int `json:"transaction_id"`
+	CustomerID    int `json:"customer_id"`
+	StatusID      int `json:"status_id"`
+	Quantity      int `json:"quantity"`
+	Amount        int `json:"amount"`
 }
 
 // Status is a type for order statuses
 type Status struct {
-	ID        int       `json:"id"`
-	Name      int       `json:"name"`
-	CreatedAt time.Time `json:"-"`
-	UpdatedAt time.Time `json:"-"`
+	DBEntity
+	Name int `json:"name"`
 }
 
 // TransactionStatus is a type for transaction statuses
 type TransactionStatus struct {
-	ID        int       `json:"id"`
-	Name      int       `json:"name"`
-	CreatedAt time.Time `json:"-"`
-	UpdatedAt time.Time `json:"-"`
+	DBEntity
+	Name int `json:"name"`
 }
 
 // Transactionis a type for transactions
 type Transaction struct {
-	ID                  int       `json:"id"`
-	Amount              int       `json:"amount"`
-	Currency            string    `json:"currency"`
-	LastFour            string    `json:"last_four"`
-	BankReturnCode      string    `json:"bank_return_code"`
-	TransactionStatusID int       `json:"transaction_status_id"`
-	CreatedAt           time.Time `json:"-"`
-	UpdatedAt           time.Time `json:"-"`
+	DBEntity
+	Amount              int    `json:"amount"`
+	Currency            string `json:"currency"`
+	LastFour            string `json:"last_four"`
+	ExpiryMonth         int    `json:"expiry_month"`
+	ExpiryYear          int    `json:"expiry_year"`
+	BankReturnCode      string `json:"bank_return_code"`
+	TransactionStatusID int    `json:"transaction_status_id"`
 }
 
 // User is a type for users
 type User struct {
-	ID        int       `json:"id"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
-	Email     string    `json:"email"`
-	Password  string    `json:"password"`
-	CreatedAt time.Time `json:"-"`
-	UpdatedAt time.Time `json:"-"`
+	DBEntity
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
 }
 
 // Customer is a type for customers
 type Customer struct {
-	ID        int       `json:"id"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
-	Email     string    `json:"email"`
-	CreatedAt time.Time `json:"-"`
-	UpdatedAt time.Time `json:"-"`
+	DBEntity
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
 }
 
+// GetWidget fetches Widget entity from DB by id
 func (m *DBModel) GetWidget(id int) (Widget, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
 	var widget Widget
-
-	tx := m.DB.WithContext(ctx)
-
-	if err := tx.First(&widget, id).Error; err != nil {
-		return widget, fmt.Errorf("error reading widget from DB: %w", err)
-	}
-
-	return widget, nil
+	err := getEntity(id, m, &widget)
+	return widget, err
 }
 
 // InsertTransaction inserts new transaction and returns it's id
 func (m *DBModel) InsertTransaction(txn Transaction) (int, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
-	tx := m.DB.WithContext(ctx).Create(txn)
-	if err := tx.Error; err != nil {
-		return 0, fmt.Errorf("error adding transaction: %w", err)
-	}
-	return int(txn.ID), nil
+	return insertEntity(txn, m)
 }
 
 // InsertOrder inserts new order and returns it's id
 func (m *DBModel) InsertOrder(order Order) (int, error) {
+	return insertEntity(order, m)
+}
+
+// InsertCustomer inserts new order and returns it's id
+func (m *DBModel) InsertCustomer(customer Customer) (int, error) {
+	return insertEntity(customer, m)
+}
+
+func insertEntity(entity IDBEntity, m *DBModel) (int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	tx := m.DB.WithContext(ctx).Create(order)
+	tx := m.DB.WithContext(ctx).Create(entity)
 	if err := tx.Error; err != nil {
-		return 0, fmt.Errorf("error adding order: %w", err)
+		return 0, fmt.Errorf("error adding %s: %w", reflect.TypeOf(entity), err)
 	}
 
-	return int(order.ID), nil
+	return entity.GetID(), nil
+}
+
+func getEntity(id int, m *DBModel, entity any) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	tx := m.DB.WithContext(ctx)
+
+	typeName := reflect.TypeOf(entity).String()
+	if err := tx.First(entity, id).Error; err != nil {
+		return fmt.Errorf("error reading %s from DB: %w", typeName, err)
+	}
+
+	return nil
 }
