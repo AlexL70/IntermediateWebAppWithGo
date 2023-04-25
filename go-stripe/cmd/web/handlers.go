b58738f1160cb2f8a3995cb75f96d43995f93961
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-04-25 09:52:56 +0300

    98. Password resets

--- go-stripe/cmd/web/handlers.go
+++ go-stripe/cmd/web/handlers.go
@@ -314,3 +314,10 @@ func (app *application) Logout(w http.ResponseWriter, r *http.Request) {
 	app.Session.RenewToken(r.Context())
 	http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
+
+func (app *application) ForgotPassword(w http.ResponseWriter, r *http.Request) {
+	if err := app.renderTemplate(w, r, "forgot-password", &templateData{}); err != nil {
+		app.errorLog.Println(fmt.Errorf("error rendering template: %w", err))
+		return
+	}
+}
