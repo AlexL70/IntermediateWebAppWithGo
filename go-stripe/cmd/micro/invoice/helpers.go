7537a6bf968c1a1ede4379b036b0ba3aefbd298c
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-05-12 18:53:19 +0300

    162. Receiving data with our micrsoservice

---
+++ go-stripe/cmd/micro/invoice/helpers.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type responsePayload struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+}
+
+// writeJson writes arbitrary data out (to response writer) as json
+func (app *application) writeJson(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
+	out, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error marshalling payload: %w", err)
+	}
+
+	if len(headers) > 0 {
+		for k, v := range headers[0] {
+			w.Header()[k] = v
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(out)
+	return nil
+}
+
+// readJSON reads json request body into data. It only accepts single json value in the body
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, data any) error {
+	maxBytes := 1048576
+
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(data)
+	if err != nil {
+		return fmt.Errorf("error decoding request body: %w", err)
+	}
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("error decoding request body: it must only have a single JSON value")
+	}
+
+	return nil
+}
+
+func (app *application) BadRequest(w http.ResponseWriter, r *http.Request, err error) error {
+	payload := responsePayload{
+		Error:   true,
+		Message: err.Error(),
+	}
+
+	return app.writeJson(w, http.StatusBadRequest, payload)
+}
+
+func (app *application) internalError(w http.ResponseWriter) error {
+	payload := responsePayload{
+		Error:   true,
+		Message: "internal server error; if it repeats, please contact the support",
+	}
+	return app.writeJson(w, http.StatusInternalServerError, payload)
+}
--- go-stripe/cmd/micro/invoice/invoice-handlers.go
+++ go-stripe/cmd/micro/invoice/invoice-handlers.go
@@ -3,8 +3,41 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
+// Order is the type for all orders
+type Order struct {
+	ID        int       `json:"id"`
+	StatusID  int       `json:"status_id"`
+	Quantity  int       `json:"quantity"`
+	Amount    int       `json:"amount"`
+	Product   string    `json:"product"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
 func (app *application) CreateAndSendInvoice(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", "world")
+	// receive json
+	var order Order
+	err := app.readJSON(w, r, &order)
+	if err != nil {
+		app.errorLog.Println(err)
+		app.BadRequest(w, r, err)
+		return
+	}
+	// generate a pdf invoice
+	// create mail
+	// send the mail with an attachment
+	// send response
+	resp := responsePayload{
+		Error:   false,
+		Message: fmt.Sprintf("Invoice %d.pdf has been created and sent to %s.", order.ID, order.Email),
+	}
+	err = app.writeJson(w, http.StatusOK, resp)
+	if err != nil {
+		app.errorLog.Println(err)
+	}
 }
